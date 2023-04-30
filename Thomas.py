20ebc3b2a785b3f0e5c4bdcedde227cd92f0f649
thomasgaudin <78448417+thomasgaudin-astro@users.noreply.github.com>
2023-04-30 14:50:17 -0400

    Added load_opacities() function

--- Thomas.py
+++ Thomas.py
@@ -1,1 +1,18 @@
+import pandas as pd
+import numpy as np
 
+def load_opacities(tab):
+    
+    opacities = pd.read_csv(tab, header=0, index_col=0)
+    
+    opacity = opacities.fillna(value=0)
+    opacity = 10**(opacity.loc[:7.5,:"-1.5"])
+    
+    log_T = 10**(np.array(opacity.index))
+    
+    log_R = (np.zeros(len(opacity.columns)))
+
+    for val, ind in zip(opacity.columns, range(len(opacity.columns))):
+        log_R[ind] = 10**float(val)
+        
+    return opacity, log_T, log_R
