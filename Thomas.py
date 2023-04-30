f78b4f376a6a7ee733abc87526a837a5d477da89
thomasgaudin <78448417+thomasgaudin-astro@users.noreply.github.com>
2023-04-30 15:09:59 -0400

    Added calc_beta() and calc_Nabla() functions

--- Thomas.py
+++ Thomas.py
@@ -16,3 +16,19 @@ def load_opacities(tab):
         log_R[ind] = 10**float(val)
         
     return opacity, log_T, log_R
+
+def calc_beta(T, rho):
+    
+    beta = ((rho * kb * T) / (mu * mH)) * (1 / (((rho * kb * T) / (mu * mH)) + (a / 3)*(T**4)))
+    
+    return beta
+
+def calc_Nabla(beta, kappa, L, P, M, T):
+    
+    Nabla_ad = (1 + ((1-beta)*(4+beta))/(beta**2)) / ((5/2) + (4*(1-beta)*(4+beta))/(beta**2))
+
+    Nabla_rad = (3 * kappa * L * P) / (16 * pi * a * c * G * M * (T**4))
+
+    Nabla = min(Nabla_ad, Nabla_rad)
+    
+    return Nabla
