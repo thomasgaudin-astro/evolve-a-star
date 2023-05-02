d423179e317f043f2ee6b1e59c0b23c2a82950a0
thomasgaudin <78448417+thomasgaudin-astro@users.noreply.github.com>
2023-05-02 09:31:37 -0400

    Add calc_zeta, calc_f, and eps_3alpha functions

--- energy_generation.py
+++ energy_generation.py
@@ -1,6 +1,33 @@
 import numpy as np
 import pandas as pd
 
+def calc_zeta(Zi, xi, Ai):
+    #Zi - proton number
+    #xi - abundance
+    #Ai - Reduced Mass
+    
+    zeta_i = []
+    
+    for Z, x, A in zip(Zi, xi, Ai):
+        
+        zet = ( ((Zi) * (Zi + 1)) / Ai ) * xi
+    
+        zeta_i.append(zet)
+        
+    zeta = sum(zeta)
+    
+    return zeta
+
+def calc_f(Z1, Z2, zeta, rho, T): 
+    
+    T6 = 1e-6 * T 
+    
+    zet_rho = (zeta * rho) / (T6**3)
+    
+    f = exp(0.188 * Z1 * Z2 * (zet_rho**(1/2)))
+    
+    return f
+
 def eps_ppc(X, T, rho):
     
     T9 = 1e-9 * T 
@@ -17,6 +44,14 @@ def eps_cno(X, Z, T, rho):
     
     return eps_c
 
-def eps_3alph(T, Y, rho, ):
+def eps_3alph(T, Y, rho, Z_alph, Z_Be, f1, f2):
     
+    T9 = 1e-9 * T 
+    
+    rho_y_T = ( ( (rho**2) * (Y**3) ) / (T9**3) )
+    
+    eps_3a = (5.08e8)*f1*f2*rho_y_T*exp(-4.4027/T9)
+    
+    return eps_3a
+
     
